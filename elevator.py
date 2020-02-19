4e90edc20ee7ebd345b258232a41f01635b8b794
clifflinrichie <lincf2000@gmail.com>
2020-02-19 18:03:19 -0500

    First working elevator

--- elevator.py
+++ elevator.py
@@ -1,38 +1,35 @@
 # Elevator Program using two queues
 # 2/19/2020
-from queue import Queue 
- # did this work 
-# Initializing a queue 
-q = Queue(maxsize = 3) 
-  
-# qsize() give the maxsize  
-# of the Queue  
-print(q.qsize())  
-  
-# Adding of element to queue 
-q.put('a') 
-q.put('b') 
-q.put('c') 
 
-#change
-print("Queue is: ")
-print(list(q.queue))
-  
-# Return Boolean for Full  
-# Queue  
-print("\nFull: ", q.full())  
-  
-# Removing element from queue 
-print("\nElements dequeued from the queue") 
-print(q.get()) 
-print(q.get()) 
-print(q.get()) 
-  
-# Return Boolean for Empty  
-# Queue  
-print("\nEmpty: ", q.empty()) 
-  
-q.put(1) 
-print("\nEmpty: ", q.empty())  
-print("Full: ", q.full()) 
+from queue import Queue 
+
+# each floor's occupation
+arrFloor = [0, 0, 0, 0, 0, 0]
+
+# elevator queue
+e1 = Queue(maxsize = 0)
+# where the elevator currently resides
+e1_index = 0
+
+def travel_to_floor(f) :
+    global e1_index
+    global arrFloor
+    # subtract by one since index of first floor is 1
+    f = int(f) - 1
+
+    while e1_index != f :
+        # add 1 to make it understandable
+        print("Passing through floor: ", e1_index + 1)
+        e1_index += 1
+
+    # change the occupation, show that one elevator currently resides at 5th floor
+    arrFloor[e1_index] = arrFloor[e1_index] + 1
+    print("Arrived at floor ", e1_index + 1)
+    
+    
+
+request = input("Enter elevator floor: ")
+travel_to_floor(request)
+print("Floor occupation", arrFloor)
+
 
